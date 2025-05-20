636c66b06f0e9f56317130d522b0fc8b8a4d0ec4
gelbel83 <pewnegorazukinga@gmail.com>
2025-05-20 17:10:39 +0200

    Utworzenie projektu graficznego, prawie pustego projektu wpf

---
+++ projekt/moja-lista-wpf/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace moja_lista_wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
